dc21539fab28cb30228f0d8df8b1d1702bdbf6b0
Drigster <gabriel.j6e@gmail.com>
2024-02-27 21:49:18 +0200

    add interaction system

--- Scripts/Chest.cs
+++ Scripts/Chest.cs
@@ -1,15 +1,25 @@
 using Godot;
-using System;
 
-public partial class Chest : StaticBody2D
+public partial class Chest : StaticBody2D, IInteractable
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	AnimationPlayer animationPlayer;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public IInteractable.InteractableType Type => IInteractable.InteractableType.Storrage;
+
+    public override void _Ready()
+    {
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
+    public void Interact(Interactor interactor, out bool interactionSuccesful)
+    {
+		animationPlayer.Play("Open");
+
+		interactionSuccesful = true;
+    }
+
+    public void EndInteraction()
+    {
+		animationPlayer.Play("Close");
+    }
 }
--- Scripts/Interactor.cs
+++ Scripts/Interactor.cs
@@ -1,136 +1,51 @@
-using System;
+using System.Linq;
 using Godot;
 using Godot.Collections;
 
-public partial class Interactor : Node
+[GlobalClass]
+public partial class Interactor : Area2D
 {
-    [Export] private Transform _interactionPoint;
-    [Export] private LayerMask _interactionLayer;
-    [Export] private float _interactionRadius = 1f;
-    private List<IInteractable> _interactablesInRadius;
-    private GameObject _closestInteractable;
-    private IInteractable _currentInteractable;
+    private Node2D _activeInteractableNode;
+    private IInteractable _activeInteractable => (IInteractable)_activeInteractableNode;
+    private IInteractable _interactingWith;
     private Player _player;
 
-    [Export] private Material _outlineMaterial;
-    private Material _originalMaterial;
-
-    private PlayerInput _playerInput;
-
-    public bool IsInteracting { get; private set; }
-    public Player Player => _player;
-
-    private void Awake()
-    {
-        _player = GetComponentInParent<Player>();
-        _playerInput = new PlayerInput();
-        _interactablesInRadius = new List<IInteractable>();
-    }
-
-    private void OnEnable()
-    {
-        _playerInput.Player.Interact.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _playerInput.Player.Interact.Disable();
-    }
-
-    private void Update()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(_interactionPoint.position, _interactionRadius);
-
-        _interactablesInRadius.Clear();
-        GameObject oldClosestInteractable = _closestInteractable;
-        _closestInteractable = null;
-
-        if (colliders.Length > 0)
-        {
-            float closestDistance = _interactionRadius + 1;
-
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                var interactable = colliders[i].GetComponent<IInteractable>();
-
-                if (interactable != null)
-                {
-                    _interactablesInRadius.Add(interactable);
-
-                    float currentDistance = Vector2.Distance(_interactionPoint.position, colliders[i].gameObject.transform.position);
-                    if (currentDistance < closestDistance)
-                    {
-                        closestDistance = currentDistance;
-                        _closestInteractable = colliders[i].gameObject;
-                    }
-                }
+    public void OnBodyEntered(Node2D body){
+        if(body is IInteractable){
+            if(_activeInteractableNode == null){
+                _activeInteractableNode = body;
+                return;
             }
 
-            if (_playerInput.Player.Interact.WasPerformedThisFrame())
-            {
-                if (IsInteracting)
-                {
-                    EndInteraction(_currentInteractable);
-                }
-                if (_closestInteractable != null)
-                {
-                    StartInteraction(_closestInteractable.GetComponent<IInteractable>());
-                }
+            if(Position.DistanceTo(body.Position) < Position.DistanceTo(_activeInteractableNode.Position)){
+                _activeInteractableNode = body;
             }
         }
-        if (_closestInteractable != null)
-        {
-            if (oldClosestInteractable != null)
-            {
-                oldClosestInteractable.GetComponent<SpriteRenderer>().material = _originalMaterial;
-                _originalMaterial = _closestInteractable.GetComponent<SpriteRenderer>().material;
-                if(_closestInteractable.GetComponent<IInteractable>().Type == IInteractable.InteractableType.Resource)
-                {
-                    _outlineMaterial.SetColor("_OutlineColor", Color.cyan);
-                }
-                else
-                {
-                    _outlineMaterial.SetColor("_OutlineColor", Color.green);
-                }
-                _closestInteractable.GetComponent<SpriteRenderer>().material = _outlineMaterial;
-            }
-            else
-            {
-                _originalMaterial = _closestInteractable.GetComponent<SpriteRenderer>().material;
-                if (_closestInteractable.GetComponent<IInteractable>().Type == IInteractable.InteractableType.Resource)
-                {
-                    _outlineMaterial.SetColor("_OutlineColor", Color.cyan);
-                }
-                else
-                {
-                    _outlineMaterial.SetColor("_OutlineColor", Color.green);
-                }
-                _closestInteractable.GetComponent<SpriteRenderer>().material = _outlineMaterial;
-            }
-        }
-        else if(oldClosestInteractable != null)
-        {
-            oldClosestInteractable.GetComponent<SpriteRenderer>().material = _originalMaterial;
-            _originalMaterial = null;
+    }
+
+    public void OnBodyExited(Node2D body){
+        if(body is not IInteractable){
+            return;
         }
 
-        if (!_interactablesInRadius.Contains(_currentInteractable) && _currentInteractable != null)
-        {
-            EndInteraction(_currentInteractable);
+        if(_activeInteractableNode == body){
+            _activeInteractableNode = null;
+        }
+        if(_interactingWith != null && _interactingWith == (IInteractable)body){
+            _interactingWith.EndInteraction();
+            _interactingWith = null;
         }
     }
 
-    void StartInteraction(IInteractable interactable)
+    public override void _Input(InputEvent @event)
     {
-        interactable.Interact(this, out bool interactSuccesful);
-        IsInteracting = interactSuccesful;
-        _currentInteractable = interactable;
-    }
+        if(@event.IsActionPressed("Interact") && _activeInteractable != null){
+            bool isSuccesfull;
+            _activeInteractable.Interact(this, out isSuccesfull);
 
-    void EndInteraction(IInteractable interactable)
-    {
-        interactable.EndInteraction();
-        IsInteracting = false;
-        _currentInteractable = null;
+            if(isSuccesfull){
+                _interactingWith = _activeInteractable;
+            }
+        }
     }
 }
